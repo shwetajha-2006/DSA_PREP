0a5a074b3d7c298aa739c8e1589faa3cf3693393
Ika Febriana Hutapea <jhashweta2006@gmail.com>
2026-04-14 23:36:44 +0530

    Time: 0 ms (100%), Space: 44.6 MB (56.03%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,34 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        
+        reverse(nums, i + 1);
+    }
+    
+    private void reverse(int[] nums, int start) {
+        int left = start, right = nums.length - 1;
+        
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
